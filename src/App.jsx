898e4def8d72345619df922f04db2738b383383c
Sonu Kachhap <166591588+Sonu-77@users.noreply.github.com>
2024-11-16 03:54:18 +0000

    Router initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import { Routes,Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Profile from './components/Profile'
 import Payment from './components/Payment'
+
 
 function App() {
 
@@ -10,9 +12,20 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
-      {/* <Home></Home> */}
-      {/* <Profile></Profile> */}
-      <Payment></Payment>
+      <Routes>
+        <Route path='/' element={<Home></Home>} />
+        <Route path='/profiles' element={<Profile></Profile>} />
+        <Route path='/payment' element={ <Payment></Payment>} />
+        <Route
+          path="*"
+          element={
+            <div className="lg:ml-[40vw] text-[#a8dadc] lg:mt-[20vw] lg:text-[3vw] capitalize">
+              404 page not found
+            </div>
+          }/>
+      </Routes>
+      
+     
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { IoHome } from "react-icons/io5";
 import { MdCall } from "react-icons/md";
 import { MdOutlineEmail } from "react-icons/md";
@@ -6,6 +7,12 @@ import { AiOutlineLogout } from "react-icons/ai";
 import { SlSettings } from "react-icons/sl";
 
 function Navbar() {
+  const activateBg = ({ isActive }) => {
+    return {
+      color: isActive && "#e3d115",
+    };
+  };
+
   return (
     <div
       className="h-[10vw] w-[100vw]  flex flex-col items-end justify-between pr-[1vw] "
@@ -18,13 +25,13 @@ function Navbar() {
           <div className="h-[2vw] w-[2vw] rounded-full border-[0.08vw] border-solid border-[#14b0b0] text-[#14b0b0] justify-center items-center flex   ">
             <IoHome />
           </div>
-          <div >
+          <div>
             <h4 className="text-[1vw] font-medium">address</h4>
             <p className="text-[0.7vw]">102 complex,259</p>
           </div>
         </div>
         <div className="flex justify-center items-center gap-[0.7vw] h-[3vw] w-[10vw] ">
-          <div className="h-[2vw] w-[2vw] rounded-full border-[0.08vw] border-solid border-[#14b0b0] text-[#14b0b0] justify-center items-center flex   " >
+          <div className="h-[2vw] w-[2vw] rounded-full border-[0.08vw] border-solid border-[#14b0b0] text-[#14b0b0] justify-center items-center flex   ">
             <MdCall />
           </div>
           <div>
@@ -33,7 +40,7 @@ function Navbar() {
           </div>
         </div>
         <div className="flex justify-center items-center gap-[0.7vw] h-[3vw] w-[10vw]">
-          <div className="h-[2vw] w-[2vw] rounded-full border-[0.08vw] border-solid border-[#14b0b0] text-[#14b0b0] justify-center items-center flex   "> 
+          <div className="h-[2vw] w-[2vw] rounded-full border-[0.08vw] border-solid border-[#14b0b0] text-[#14b0b0] justify-center items-center flex   ">
             <MdOutlineEmail />
           </div>
           <div>
@@ -44,23 +51,37 @@ function Navbar() {
       </div>
       <div className="h-[4vw] w-[80vw] bg-[#14b0b0] flex gap-[4vw] rounded-md text-[#ffffff] ">
         <div className="flex h-[100%] w-[70%] items-center justify-around  text-[1vw]">
+          <NavLink style={activateBg} to={"/"}>
             <h2>Home</h2>
+          </NavLink>
+          <NavLink style={activateBg} to={"/profiles"}>
             <h2>Profile</h2>
+          </NavLink>
+          <NavLink style={activateBg} to={"/payment"}>
             <h2>Payment</h2>
+          </NavLink>
+          <NavLink>
             <h2>Order Fuel</h2>
+          </NavLink>
+          <NavLink>
             <h2>Vehicles Details</h2>
+          </NavLink>
+          <NavLink>
             <h2>Driver Details</h2>
+          </NavLink>
+          <NavLink>
             <h2>Roles</h2>
+          </NavLink>
         </div>
         <div className="flex items-center justify-around gap-[3vw] ml-[2vw] text-[1vw]">
-            <div className="flex items-center justify-center gap-[0.5vw]">
-                <AiOutlineLogout />
-                <h2>Exit Profile</h2>
-            </div>
-            <div className="flex items-center justify-center gap-[0.5vw]">
-                <SlSettings />
-                <h2>Settings</h2>
-            </div>
+          <div className="flex items-center justify-center gap-[0.5vw]">
+            <AiOutlineLogout />
+            <h2>Exit Profile</h2>
+          </div>
+          <div className="flex items-center justify-center gap-[0.5vw]">
+            <SlSettings />
+            <h2>Settings</h2>
+          </div>
         </div>
       </div>
     </div>
